003c231e2a191cc15075669ce0f828f02ca567b2
ungredda95@gmail.com <ungredda95@gmail.com>
2022-12-08 12:00:12 -0400

    Fix Linters Again

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -26,7 +26,7 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
-    
+
     return unless user.present?
 
     can :read, :all
